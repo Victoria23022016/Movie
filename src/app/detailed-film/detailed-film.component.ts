6b6959378f31136e3d789d7604847b85ed3ec356
Vika <svi.victoria@gmail.com>
2023-08-13 19:36:06 +0700

    Добавляю функционал Favourites

--- src/app/detailed-film/detailed-film.component.ts
+++ src/app/detailed-film/detailed-film.component.ts
@@ -6,6 +6,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Observable } from 'rxjs';
 import { PostsComponent } from '../posts/posts.component';
 import { ReccomendatedFilm } from '../data/interfaces.service';
+import { LocalStorage } from '../data/localstorage.service';
 
 @Component({
   selector: 'app-detailed-film',
@@ -15,8 +16,12 @@ import { ReccomendatedFilm } from '../data/interfaces.service';
 export class DetailedFilmComponent implements OnInit {
   popularFilms: any; //почему не могу задать интерфейс Film?
   film: any; //почему не могу задать интерфейс Film?
-
-  constructor(public DataService: DataService, private route: ActivatedRoute) {}
+  activatedBtn = false;
+  constructor(
+    public DataService: DataService,
+    private route: ActivatedRoute,
+    public LocalStorage: LocalStorage
+  ) {}
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       const id = params.get('id');
@@ -25,5 +30,10 @@ export class DetailedFilmComponent implements OnInit {
           this.film = film;
         });
     });
+  }
+  AddToFavourites(film: any): void {
+    this.LocalStorage.listOfFavourites.push(film);
+    console.log(`Добавили в фавориты ${this.LocalStorage.listOfFavourites}`);
+    this.activatedBtn = !this.activatedBtn;
   }
 }
--- src/app/favourites/favourites.component.ts
+++ src/app/favourites/favourites.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { LocalStorage } from '../data/localstorage.service';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.component.html',
-  styleUrls: ['./favourites.component.scss']
+  styleUrls: ['./favourites.component.scss'],
 })
 export class FavouritesComponent {
-
+  constructor(public LocalStorage: LocalStorage) {}
+  RemoveFromFavourites(film: any): void {
+    console.log('Film id:', film.id);
+    let spliceId = this.LocalStorage.listOfFavourites.findIndex(
+      (el: any) => el.id == film.id
+    );
+    console.log(spliceId);
+    this.LocalStorage.listOfFavourites.splice(spliceId, 1);
+    console.log(this.LocalStorage.listOfFavourites);
+  }
 }
